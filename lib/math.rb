d21693d5e2dbb9ebb4a69526a186911b214e85ab
allison Coombs <allison@allisoncoombs.com>
2019-07-12 03:28:17 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -1,6 +1,5 @@
 def addition(num1, num2)
-sum = num1 + num2
-puts sum
+
 end
 
 def subtraction(num1, num2)
@@ -20,5 +19,5 @@ def modulo(num1, num2)
 end
 
 def square_root(num)
-puts math.sqrt
+
 end
